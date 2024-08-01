78900819f2fcc97d1f8db4589ea753478d00a911
lemus <lemus777@gmail.com>
2024-08-01 13:25:34 +0200

    get categories from API and validating them with zod

--- src/services/RecipeService.ts
+++ src/services/RecipeService.ts
@@ -1,4 +1,12 @@
+import axios from "axios"
+import { CategoriesAPIResponseSchema } from "../utils/recipes-schema"
 
 export async function getCategories() {
-  console.log('Desde RecipeService')
+  const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list'
+  const { data } = await axios(url)
+  const result = CategoriesAPIResponseSchema.safeParse(data)
+
+  if (result.success) {
+    return result.data
+  }
 }

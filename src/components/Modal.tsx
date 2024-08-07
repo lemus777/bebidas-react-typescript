7a145433528e6e23799e29cb096195c687a20ed9
lemus777 <lemus777@gmail.com>
2024-08-07 17:25:41 +0200

    dynamic text for button add/delete from fav

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -9,6 +9,7 @@ export default function Modal() {
   const closeModal = useAppStore((state) => state.closeModal)
   const selectedRecipe = useAppStore((state) => state.selectedRecipe)
   const handleClickFavorite = useAppStore((state) => state.handleClickFavorite)
+  const favoriteExists = useAppStore((state) => state.favoriteExists)
 
   const renderIngredients = () => {
     const ingredients : JSX.Element[] = []
@@ -81,7 +82,7 @@ export default function Modal() {
                       type='button'
                       className='w-full rounded bg-orange-600 p-3 font-bold uppercase text-white shadow hover:bg-orange-500'
                       onClick={() => handleClickFavorite(selectedRecipe)}
-                    >Agregar a favoritos</button>
+                    >{favoriteExists(selectedRecipe.idDrink) ? 'Eliminar favorito' : 'Agregar a favoritos'}</button>
                   </div>
                 </Dialog.Panel>
               </Transition.Child>

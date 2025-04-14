020198f8fe9132e2805ea70b03d1c3cb4501c37b
lemus <lemus777@gmail.com>
2025-04-14 12:02:06 +0200

    AI slice created

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -3,9 +3,11 @@ import { devtools } from 'zustand/middleware'
 import { createRecipesSlice, RecipesSliceType } from './recipeSlice'
 import { FavoritesSliceType, createFavoritesSlice } from './favoritesSlice'
 import { NotificationSliceType, createNotificationSlice } from './notificationSlice'
+import { AISlice, createAISlice } from './aiSlice'
 
-export const useAppStore = create<RecipesSliceType & FavoritesSliceType & NotificationSliceType>()(devtools((...a) => ({
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType & NotificationSliceType & AISlice>()(devtools((...a) => ({
   ...createRecipesSlice(...a),
   ...createFavoritesSlice(...a),
   ...createNotificationSlice(...a),
+  ...createAISlice(...a)
 })))

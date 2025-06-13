918b041f749f3a07f637d5051ae847fb3bc1b216
lemus <lemus777@gmail.com>
2025-06-13 13:02:17 +0200

    last tweaks

--- src/stores/aiSlice.ts
+++ src/stores/aiSlice.ts
@@ -3,17 +3,21 @@ import AIService from "../services/AIService"
 
 export type AISlice = {
   recipe: string
+  isGenerating: boolean
   generateRecipe: (prompt: string) => Promise<void>
 }
 
-export const createAISlice : StateCreator<AISlice, [], [], AISlice> = (set) => ({
+export const createAISlice : StateCreator<AISlice> = (set) => ({
   recipe: '',
+  isGenerating: false,
   generateRecipe: async (prompt) => {
+    set({recipe: '', isGenerating: true})
     const data = await AIService.generateRecipe(prompt)
     for await (const textPart of data) {
       set((state => ({
         recipe: state.recipe + textPart
       })))
     }
+    set({ isGenerating: false })
   }
 })
